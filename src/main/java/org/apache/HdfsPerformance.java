f93b39896236d57889079baa9a9752d8e2ceb99f
captainzmc <captainzmc@apache.org>
2022-05-31 14:17:27 +0800

    Single-thread test HDFS read and write

--- src/main/java/org/apache/HdfsPerformance.java
+++ src/main/java/org/apache/HdfsPerformance.java
@@ -1,6 +1,7 @@
 package org.apache;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,6 +9,7 @@ import org.apache.hadoop.fs.Path;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URI;
@@ -24,6 +26,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.hadoop.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,63 +34,8 @@ public class HdfsPerformance {
   private static long numFiles;
   private static long fileSizeInBytes = 128000000;
   private static long bufferSizeInBytes = 4000000;
-  private static String[] storageDir = new String[]{"/data/ratis/", "/data1/ratis/", "/data2/ratis/", "/data3/ratis/",
-      "/data4/ratis/", "/data5/ratis/", "/data6/ratis/", "/data7/ratis/", "/data8/ratis/", "/data9/ratis/",
-      "/data10/ratis/", "/data11/ratis/"};
 
   public static final Logger LOG = LoggerFactory.getLogger(HdfsPerformance.class);
-
-  private static void createDirs() throws IOException {
-    for (String dir : storageDir) {
-      Files.createDirectory(new File(dir).toPath());
-    }
-  }
-
-  public static String getPath(String fileName) {
-    int hash = fileName.hashCode() % storageDir.length;
-    return new File(storageDir[Math.abs(hash)], fileName).getAbsolutePath();
-  }
-
-  protected static void dropCache() throws InterruptedException, IOException {
-    String[] cmds = {"/bin/sh","-c","echo 3 > /proc/sys/vm/drop_caches"};
-    Process pro = Runtime.getRuntime().exec(cmds);
-    pro.waitFor();
-  }
-
-  private static CompletableFuture<Long> writeFileAsync(String path, ExecutorService executor) {
-    final CompletableFuture<Long> future = new CompletableFuture<>();
-    CompletableFuture.supplyAsync(() -> {
-      try {
-        future.complete(
-            writeFile(path, fileSizeInBytes, bufferSizeInBytes, new Random().nextInt(127) + 1));
-      } catch (IOException e) {
-        future.completeExceptionally(e);
-      }
-      return future;
-    }, executor);
-    return future;
-  }
-
-  protected static List<String> generateFiles(ExecutorService executor) {
-    UUID uuid = UUID.randomUUID();
-    List<String> paths = new ArrayList<>();
-    List<CompletableFuture<Long>> futures = new ArrayList<>();
-    for (int i = 0; i < numFiles; i ++) {
-      String path = getPath("file-" + uuid + "-" + i);
-      paths.add(path);
-      futures.add(writeFileAsync(path, executor));
-    }
-
-    for (int i = 0; i < futures.size(); i ++) {
-      long size = futures.get(i).join();
-      if (size != fileSizeInBytes) {
-        System.err.println("Error: path:" + paths.get(i) + " write:" + size +
-            " mismatch expected size:" + fileSizeInBytes);
-      }
-    }
-
-    return paths;
-  }
 
   protected static long writeFile(String path, long fileSize, long bufferSize, int random) throws IOException {
     RandomAccessFile raf = null;
@@ -112,84 +60,61 @@ public class HdfsPerformance {
     return offset;
   }
 
-  private static Map<String, CompletableFuture<Boolean>> writeByHeapByteBuffer(
-      List<String> paths, List<FSDataOutputStream> outs, ExecutorService executor) {
-    Map<String, CompletableFuture<Boolean>> fileMap = new HashMap<>();
+  private static void writeToHDFS(FileSystem fs, String srcPath,Path dstPath)
+      throws IOException {
+    long start = System.currentTimeMillis();
+    FSDataOutputStream out = fs.create(dstPath);
+    long end1 = System.currentTimeMillis();
+    File file = new File(srcPath);
+    FileInputStream in = new FileInputStream(file);
+    IOUtils.copyBytes(in, out, 4096,true);
+    long end2 = System.currentTimeMillis();
+    out.close();
+    long end3 = System.currentTimeMillis();
+    System.out.println(srcPath+" meta cost: "+((end1-start)+(end3-end2)));
+    System.out.println(srcPath+" write cost: "+(end2-end1));
+    System.out.println(srcPath+" total cost: "+(end3-start));
+  }
 
-    for(int i = 0; i < paths.size(); i ++) {
-      String path = paths.get(i);
-      FSDataOutputStream out = outs.get(i);
-      final CompletableFuture<Boolean> future = new CompletableFuture<>();
-      CompletableFuture.supplyAsync(() -> {
-        File file = new File(path);
-        try (FileInputStream fis = new FileInputStream(file)) {
-          int bytesToRead = (int)bufferSizeInBytes;
-          byte[] buffer = new byte[bytesToRead];
-          long offset = 0L;
-          while(fis.read(buffer, 0, bytesToRead) > 0) {
-            out.write(buffer);
-            offset += bytesToRead;
-            bytesToRead = (int) Math.min(fileSizeInBytes - offset, bufferSizeInBytes);
-            if (bytesToRead > 0) {
-              buffer = new byte[bytesToRead];
-            }
-          }
-          out.hsync();
-          out.close();
-          future.complete(true);
-        } catch (Throwable e) {
-          LOG.error("exception", e);
-          future.complete(false);
-        }
+  public static void ReadFile(FileSystem fs, String hdfs) throws IOException {
 
-        return future;
-      }, executor);
-
-      fileMap.put(path, future);
+    long start = System.currentTimeMillis();
+    FSDataInputStream hdfsInStream = fs.open(new Path(hdfs));
+    long end1 = System.currentTimeMillis();
+    byte[] ioBuffer = new byte[1024];
+    int readLen = hdfsInStream.read(ioBuffer);
+    while(readLen!=-1) {
+      readLen = hdfsInStream.read(ioBuffer);
     }
-    return fileMap;
+    hdfsInStream.close();
+    long end2 = System.currentTimeMillis();
+    System.out.println(hdfs+" meta cost: "+((end1-start)));
+    System.out.println(hdfs+" read cost: "+(end2-end1));
+    System.out.println(hdfs+" total cost: "+(end2-start));
+    fs.close();
   }
 
   public static void main(String[] args) {
      try {
-       numFiles = Integer.parseInt(args[0]);
-       System.out.println("numFiles:" + numFiles);
-       createDirs();
-       final ExecutorService executor = Executors.newFixedThreadPool(1000);
-
-       List<String> paths = generateFiles(executor);
-       dropCache();
-
+       String url = "hdfs://xxxxx:9000";
        Configuration conf = new Configuration();
-       conf.set("fs.defaultFS", "hdfs://localhost:9000");
+       conf.set("fs.defaultFS", url);
        FileSystem fs = FileSystem.get(conf);
 
-       List<FSDataOutputStream> outs = new ArrayList<>();
-       for (int i = 0; i < paths.size(); i ++) {
-         Path dstPath = new Path(paths.get(i)); //目标路径
-         //打开一个输出流
-         FSDataOutputStream outputStream = fs.create(dstPath);
-         outs.add(outputStream);
+       if (args.length == 1) {
+         ReadFile(fs, args[0]);
+       } else {
+         String path = "./"+ args[0];
+         fileSizeInBytes = Integer.parseInt(args[1]);
+         writeFile(path, fileSizeInBytes, bufferSizeInBytes, new Random().nextInt(127) + 1);
+         String dstString = url+"/tmp/"+args[0]+System.currentTimeMillis();
+         Path dstPath = new Path(dstString);
+         writeToHDFS(fs, path, dstPath);
        }
-
-       long start = System.currentTimeMillis();
-       // Write key with random name.
-       Map<String, CompletableFuture<Boolean>> map = writeByHeapByteBuffer(paths, outs, executor);
-
-       for (String path : map.keySet()) {
-         CompletableFuture<Boolean> future = map.get(path);
-         if (!future.join().booleanValue()) {
-           System.err.println("Error: path:" + path + " write fail");
-         }
-       }
-
-       long end = System.currentTimeMillis();
-       System.err.println("cost:" + (end - start) + " xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+       fs.close();
      } catch (Exception e) {
        System.err.println(e);
      }
 
-    System.out.println("HDFS Demo End.");
-    System.exit(0);
   }
 }
